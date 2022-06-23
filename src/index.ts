dfdf28e5a60b300e6f7a96d7d41b2f7c2bda2a76
Frederik Juulsgaard Sørensen <frederikjs1@hotmail.com>
2022-06-23 17:02:54 +0200

    Export reducer types v1.0.4

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export {StoreService} from "./store-service";
 export {LoadingState} from "./loading-state"
 export {IStoreConfigService} from "./models/store-config-service"
 export {Reducers} from './collections/reducers';
+export {ListReducer, ObjectReducer} from './models/store-types'
