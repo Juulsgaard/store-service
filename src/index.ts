2cde985779a24c8b49e3bb36b195234c20d8d24b
Frederik Sørensen <frederikjs1@hotmail.com>
2023-05-21 13:30:03 +0200

    Export Command Types

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ export {CacheStoreService} from "./cache-store-service";
 export {IStoreConfigService} from "./models/store-config-service"
 export {BaseReducers} from './collections/reducers';
 export {WhereItem} from './collections/list-selectors';
-export {ListReducer, ObjectReducer} from './models/store-types'
-export {CacheItemData, CacheAdapter, CacheTransactionAdapter} from './caching/caching-adapter'
-export {CacheDatabaseContext} from './caching/caching-interface'
-export {IndexedDbAdapter} from './caching/adapters/indexed-db-adapter'
-export * from './models/errors'
+export {ListReducer, ObjectReducer} from './models/store-types';
+export type {PayloadCommand, StoreCommand, StoreCommandUnion, ActionCommandUnion} from './models/base-commands';
+export type {ActionCommand} from './commands/action-command';
+export type {CacheCommand} from './commands/cache-command';
+export type {DeferredCommand} from './commands/deferred-command';
+export type {PlainCommand} from './commands/plain-command';
+export {CacheItemData, CacheAdapter, CacheTransactionAdapter} from './caching/caching-adapter';
+export {CacheDatabaseContext} from './caching/caching-interface';
+export {IndexedDbAdapter} from './caching/adapters/indexed-db-adapter';
+export * from './models/errors';
 
-export * from './utils/load-operators'
-export * from './utils/future-operators'
+export * from './utils/load-operators';
+export * from './utils/future-operators';
