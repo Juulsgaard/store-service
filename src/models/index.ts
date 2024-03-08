d3e15cec74e287d7045b279a3caf28b30da37da3
Frederik Sørensen <frederikjs1@hotmail.com>
2024-03-08 13:00:25 +0100

    Export new base types

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,7 @@
 export * from './errors';
 export {QueueAction} from './queue-action';
 export {ListReducer, ObjectReducer, ListSelector, Reducer} from './store-types';
-export type {PayloadCommand, StoreCommand, StoreCommandUnion, ActionCommandUnion} from './base-commands';
-export {BaseCommand} from './base-commands';
+export type {StoreCommandUnion, ActionCommandUnion} from './base-commands';
+export {BaseCommand, PayloadCommand, StoreCommand, AsyncCommand, AsyncPayloadCommand} from './base-commands';
 export {IStoreConfigService} from "./store-config-service"
 export {ReducerScope, ReducerCoalesce, rootReducerScope, objectReducerScope, listReducerScope, applyScopedObjectReducer} from "./reducer-scope"
